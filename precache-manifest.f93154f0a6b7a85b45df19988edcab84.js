80a4bcbb20d44faf1329814ac5e6f422a7add195
Andriy <powerlviv@ukr.net>
2020-09-08 21:56:42 +0300

    Updates

---
+++ precache-manifest.f93154f0a6b7a85b45df19988edcab84.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699279d9fbeb01e8fd19f8d31ab1f1a5",
+    "url": "/reactPhoneNumberBook/index.html"
+  },
+  {
+    "revision": "413a1f2c60047a3aaa70",
+    "url": "/reactPhoneNumberBook/static/css/main.43f5d6b5.chunk.css"
+  },
+  {
+    "revision": "77194d1baa3b4e3eed9a",
+    "url": "/reactPhoneNumberBook/static/js/2.61705e32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPhoneNumberBook/static/js/2.61705e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "413a1f2c60047a3aaa70",
+    "url": "/reactPhoneNumberBook/static/js/main.93ef7f78.chunk.js"
+  },
+  {
+    "revision": "eb3e68890c4a355509e7",
+    "url": "/reactPhoneNumberBook/static/js/runtime-main.715ee035.js"
+  },
+  {
+    "revision": "c981df5303becacdb08340389060a0d5",
+    "url": "/reactPhoneNumberBook/static/media/anon.c981df53.svg"
+  },
+  {
+    "revision": "c3ee4a7e57200d73f815f652fac591b3",
+    "url": "/reactPhoneNumberBook/static/media/female.c3ee4a7e.svg"
+  },
+  {
+    "revision": "2889fe11d673bb8f9e9156efea05eecf",
+    "url": "/reactPhoneNumberBook/static/media/male.2889fe11.svg"
+  },
+  {
+    "revision": "422b2b5864ed6394e895e26669ea79e7",
+    "url": "/reactPhoneNumberBook/static/media/search.422b2b58.png"
+  }
+]);
